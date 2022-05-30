8a618aa0d36a543e4ada2dc98daebef11cd89c8e
Gattaka20044 <nexti20044@mail.ru>
2022-05-31 00:27:33 +0300

    Конец 11 главы

--- app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
@@ -10,5 +10,5 @@ import com.bignerdranch.android.criminalintent.Crime
 
 abstract class CrimeDatabase : RoomDatabase() {
 
-    abstract fun crimeDao: CrimeDao
+    abstract fun crimeDao(): CrimeDao
 }
